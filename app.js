b68e5069f2ab1ef1a56ba2405eaafeeafcc31adb
bukharee <idrisbukhariauwal@gmail.com>
2021-06-24 17:25:22 +0100

    start javascript code

---
+++ app.js
@@ -0,0 +1,22 @@
+const ONE = document.getElementById('one');
+const TWO = document.getElementById('two');
+const THREE = document.getElementById('three');
+const FOUR = document.getElementById('four');
+const FIVE = document.getElementById('five');
+const SIX = document.getElementById('six');
+const SEVEN = document.getElementById('seven');
+const EIGHT = document.getElementById('eight');
+const NINE = document.getElementById('nine');
+const ZERO = document.getElementById('zero');
+const PLUS = document.getElementById('plus');
+const MINUS = document.getElementById('minus');
+const TIMES = document.getElementById('times');
+const DEVIDE = document.getElementById('devide');
+const CLEAR = document.getElementById('clear');
+const DELETE = document.getElementById('delete');
+const ON = document.getElementById('on');
+const DISPLAY = document.getElementById('display')
+
+
+
+ONE.onclick = DISPLAY.innerHTML = "1";
