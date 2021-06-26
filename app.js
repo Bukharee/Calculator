07bb1e108073699e48fc21033352ba08f6a7f6a2
bukharee <idrisbukhariauwal@gmail.com>
2021-06-26 07:10:21 +0100

    javascript calculations done

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
-const ONE = document.getElementById('one');
-const TWO = document.getElementById('two');
-const THREE = document.getElementById('three');
-const FOUR = document.getElementById('four');
-const FIVE = document.getElementById('five');
-const SIX = document.getElementById('six');
-const SEVEN = document.getElementById('seven');
-const EIGHT = document.getElementById('eight');
-const NINE = document.getElementById('nine');
-const ZERO = document.getElementById('zero');
-const PLUS = document.getElementById('plus');
-const MINUS = document.getElementById('minus');
-const TIMES = document.getElementById('times');
-const DEVIDE = document.getElementById('devide');
-const CLEAR = document.getElementById('clear');
-const DELETE = document.getElementById('delete');
-const ON = document.getElementById('on');
-const DISPLAY = document.getElementById('display')
+const DISPLAY = document.getElementById("display");
+const EQUAL = document.getElementById("equal");
+const ON = document.getElementById("on");
 
+let state = true;
+let eligible = true;
 
-
-ONE.onclick = DISPLAY.innerHTML = "1";
+function eligibility(num) {
+    if (DISPLAY.innerHTML == "0" && (num == "*" || num == "/" || num == "+")) {
+        eligible = false;
+    } else {
+        eligible = true;
+    }
+}
+function clean() {
+  if (DISPLAY.innerHTML == 0) {
+    DISPLAY.innerHTML = "";
+  }
+}
+function display(num) {
+    eligibility(num);
+  if (state && eligible) {
+    clean();
+    DISPLAY.innerHTML += num;
+  }
+}
+function wipe() {
+  if (state) {
+    // A function to reset the state of the cal to initial
+    DISPLAY.innerHTML = "0";
+  }
+}
+function answer() {
+  if (state && DISPLAY.innerHTML != "") {
+    let expression = DISPLAY.innerHTML;
+    ans = eval(expression);
+    return (DISPLAY.innerHTML = ans);
+  }
+}
+function power(params) {
+  if (state) {
+    DISPLAY.innerHTML = "";
+    state = false;
+    ON.innerHTML = "ON";
+  } else {
+    state = true;
+    DISPLAY.innerHTML = "0";
+    ON.innerHTML = "OFF";
+  }
+}
